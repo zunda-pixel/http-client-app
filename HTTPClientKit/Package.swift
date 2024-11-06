4d346ba7d9e4d865bce4cbb8ee6dfca864946409
zunda <zunda.dev@gmail.com>
2024-11-06 20:32:55 +0900

    add HTTPTypes

--- HTTPClientKit/Package.swift
+++ HTTPClientKit/Package.swift
@@ -13,9 +13,17 @@ let package = Package(
       targets: ["HTTPClientKit"]
     ),
   ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.0"),
+  ],
   targets: [
     .target(
-      name: "HTTPClientKit"),
+      name: "HTTPClientKit",
+      dependencies: [
+        .product(name: "HTTPTypes", package: "swift-http-types"),
+        .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
+      ]
+    ),
     .testTarget(
       name: "HTTPClientKitTests",
       dependencies: ["HTTPClientKit"]
