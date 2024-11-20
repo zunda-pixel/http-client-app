e75eec0534d7aeb4e7c404dab9907206792588de
zunda.dev@gmail.com <zunda.dev@gmail.com>
2024-11-20 22:28:09 +0900

    fix ResultDetailView

--- HTTPClientKit/Sources/HTTPClientKit/ResultDetailView.swift
+++ HTTPClientKit/Sources/HTTPClientKit/ResultDetailView.swift
@@ -3,7 +3,15 @@ import SwiftUI
 
 struct ResultDetailView: View {
   let result: HTTPResult
-
+  @State private var isPresentedDuration = false
+  
+  var navigationTitle: String {
+    switch result.result {
+    case .success(_): "Result"
+    case .failure(_): "Failure Result"
+    }
+  }
+  
   var body: some View {
     Form {
       Section("Information") {
@@ -15,10 +23,12 @@ struct ResultDetailView: View {
 
         LabeledContent("Start Date") {
           Text(result.startTime, formatter: dateFormatter)
+            .font(.caption)
         }
 
         LabeledContent("End Date") {
           Text(result.endTime, formatter: dateFormatter)
+            .font(.caption)
         }
 
         let duration = result.endTime.timeIntervalSince(result.startTime)
@@ -26,15 +36,26 @@ struct ResultDetailView: View {
           let duration: String = {
             let formatter = MeasurementFormatter()
             formatter.numberFormatter.maximumFractionDigits = 4
-            formatter.unitOptions = [
-              .naturalScale
-            ]
+            formatter.unitOptions = [.naturalScale]
             return formatter.string(from: .init(value: duration, unit: UnitDuration.seconds))
           }()
           Text(duration)
         }
-        .contextMenu {
-          Button(duration.description) {}
+        .contentShape(.rect)
+        .onTapGesture {
+          isPresentedDuration.toggle()
+        }
+        .popover(isPresented: $isPresentedDuration) {
+          let duration: String = {
+            let formatter = MeasurementFormatter()
+            formatter.unitOptions = [
+              .providedUnit,
+            ]
+            return formatter.string(from: .init(value: duration, unit: UnitDuration.seconds))
+          }()
+          Text(duration)
+            .padding()
+            .presentationCompactAdaptation(.popover)
         }
       }
 
@@ -46,7 +67,7 @@ struct ResultDetailView: View {
       }
     }
     .formStyle(.grouped)
-    .navigationTitle("Result")
+    .navigationTitle(navigationTitle)
   }
 }
 
@@ -59,43 +80,59 @@ extension ResultDetailView {
 
     var body: some View {
       LabeledContent("Status Code", value: response.status.code.description)
+        .foregroundStyle(response.status == .ok ? .green : .red)
 
-      Section("Headers", isExpanded: $isExpanded) {
-        Table(response.headerFields.sorted(using: KeyPathComparator(\.name.rawName))) {
-          TableColumn("Name", value: \.name.rawName)
-          TableColumn("Value", value: \.value)
+      Section {
+        if response.headerFields.isEmpty {
+          Text("No headers")
+        } else {
+          DisclosureGroup("Headers", isExpanded: $isExpanded) {
+            ForEach(response.headerFields.sorted(using: KeyPathComparator(\.name.rawName))) { header in
+              HStack {
+                Text(header.name.rawName)
+                  .bold()
+                Spacer()
+                Divider()
+                Spacer()
+                Text(header.value)
+                  .foregroundStyle(.secondary)
+                  .bold()
+              }
+            }
+          }
         }
       }
       
       Section {
-        if let string = String(data: data, encoding: .utf8) {
-          Text(string)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .overlay(alignment: .topTrailing) {
-              Button {
-                #if canImport(UIKit)
-                UIPasteboard.general.string = string
-                #elseif canImport(AppKit)
-                NSPasteboard.general.clearContents()
-                NSPasteboard.general.setString(string, forType: .string)
-                #endif
-              } label: {
-                Label("Copy", systemImage: "doc.on.doc")
-              }
-            }
-        } else {
-          Text("Failed to decode data")
-        }
-      } header: {
-        HStack {
-          Text("Data")
-          Divider()
+        if let string = String(data: data, encoding: encoding.rawEncoding) {
           Picker("Encoding", selection: $encoding) {
             ForEach(BodyEncoding.allCases, id: \.self) { encoding in
               Text(encoding.rawValue)
                 .tag(encoding)
             }
           }
+          Text(string)
+            .frame(maxWidth: .infinity, alignment: .leading)
+        } else {
+          Text("Failed to decode data")
+        }
+      } header: {
+        HStack {
+          Text("Data")
+          Spacer()
+          Button {
+            guard let string = String(data: data, encoding: encoding.rawEncoding) else { return }
+            #if canImport(UIKit)
+            UIPasteboard.general.string = string
+            #elseif canImport(AppKit)
+            NSPasteboard.general.clearContents()
+            NSPasteboard.general.setString(string, forType: .string)
+            #endif
+          } label: {
+            Label("Copy", systemImage: "doc.on.doc")
+              .font(.caption)
+          }
+          .buttonStyle(.bordered)
         }
       }
     }
@@ -104,23 +141,36 @@ extension ResultDetailView {
   struct FailureView: View {
     var error: any Error
     var body: some View {
-      Text(error.localizedDescription)
+      Section("Error") {
+        Text(error.localizedDescription)
+      }
     }
   }
 }
 
 #Preview("Success") {
-  ResultDetailView(result: .init(
-    startTime: .now,
-    endTime: .now.addingTimeInterval(123.456),
-    result: .success((.init(), .init(status: .ok)))
-  ))
+  NavigationStack {
+    ResultDetailView(result: .init(
+      startTime: .now,
+      endTime: .now.addingTimeInterval(123.456789123456789),
+      result: .success((Data("""
+{
+  "id": 1,
+  "name": "Hello, World!",
+  "age": 34,
+  "biirthdayAt": \(Date.now.timeIntervalSinceReferenceDate)
+}
+""".utf8), .init(status: .ok, headerFields: [.accept: "application/json"])))
+    ))
+  }
 }
 
 #Preview("Failure") {
-  ResultDetailView(result: .init(
-    startTime: .now,
-    endTime: .now.addingTimeInterval(123.456),
-    result: .failure(NSError(domain: "Failed something", code: 1))
-  ))
+  NavigationStack {
+    ResultDetailView(result: .init(
+      startTime: .now,
+      endTime: .now.addingTimeInterval(123.456),
+      result: .failure(NSError(domain: "Failed something", code: 1))
+    ))
+  }
 }
